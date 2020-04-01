19a88f1cd878a07eab5c59c44d19f777302c9cdf
Erin Claudio <eclaudio@gmail.com>
2020-04-01 18:51:18 -0400

    added changes to app.py/db issue

--- app.py
+++ app.py
@@ -32,8 +32,6 @@ if __name__ == '__main__':
     db.init_app(app)
 
     if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+
 
     app.run(port=5000)
